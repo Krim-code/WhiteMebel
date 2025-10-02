ffa40e1b52817df5a34a6c72b5ce933f307668f6
RiKrim <suhetsky.gricha555@gmail.com>
2025-10-02 09:32:25 +0300

    modified:   whitemebel/config/settings.py

--- whitemebel/config/settings.py
+++ whitemebel/config/settings.py
@@ -105,15 +105,15 @@ AUTH_USER_MODEL = 'core.User'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-if DEBUG:
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": BASE_DIR / "db.sqlite3",
-        }
-    }
-else:
-    DATABASES = {
+# if DEBUG:
+#     DATABASES = {
+#         "default": {
+#             "ENGINE": "django.db.backends.sqlite3",
+#             "NAME": BASE_DIR / "db.sqlite3",
+#         }
+#     }
+# else:
+DATABASES = {
         "default": {
             "ENGINE": "django.db.backends.postgresql",
             "NAME": os.getenv("DJANGO_DB_NAME", "app"),
